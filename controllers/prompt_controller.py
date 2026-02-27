56f487e81a548ad192e7c66e5e59ee566bd350be
Mariano Marra <ar.mmarra@gmail.com>
2026-02-27 17:09:55 -0300

    feat: generar_mix_repaso function

--- controllers/prompt_controller.py
+++ controllers/prompt_controller.py
@@ -30,19 +30,25 @@ def obtener_listas_dashboard(usuario_id, categoria_id):
                                .outerjoin(Progreso, and_(Progreso.tema == Topic.id, Progreso.usuario == usuario_id))
                                .filter(Topic.categoria == categoria_id).all())
         for tema, progreso in query_tema_progreso:
+            base = {'id_tema': tema.id, 'sesion_tema': tema.sesion, 'contenido_tema': tema.contenido,
+                                  'comandos_tema': tema.comandos, 'categoria_tema': tema.categoria}
+            version_pendiente = {'id_progreso': None, 'usuario_progreso': None, 'tema_progreso': None,
+                                  'fecha_progreso': None, 'repeciones_progreso': None }
+            version_historial = {'id_progreso': progreso.id, 'usuario_progreso': progreso.usuario, 'tema_progreso': progreso.tema,
+                                  'fecha_progreso': progreso.fecha, 'repeticiones_progreso': progreso.repeticiones}
             if progreso is None:
-                pendiente.append((tema, progreso))
+                pendiente.append(base | version_pendiente)
             else:
-                historial.append((tema, progreso))
-    pendiente_sorted = sorted(pendiente, key= lambda x: x[0].id)
-    historial_sorted = sorted(historial, key= lambda x: x[1].fecha, reverse=True)
+                historial.append(base | version_historial)
+    pendiente_sorted = sorted(pendiente, key= lambda x: x['id_tema'])
+    historial_sorted = sorted(historial, key= lambda x: x['fecha_progreso'], reverse=True)
     return {'pendiente': pendiente_sorted, 'historial': historial_sorted}
 
-
-# --- FUNCIÓN 2: EL MIX DE ESTUDIO (4+4) ---
-# 4. 🧠 Creá 'generar_mix_repaso(usuario_id, categoria_id)':
-#    - Buscá solo los temas que ya están en la tabla 'Progreso' para ese usuario/categoría.
-#    - Ordenalos por la fecha de práctica más reciente.
-#    - Tomá los primeros 4 (los "fijos" por ser los últimos movidos).
-#    - Del resto de la lista (del 5to en adelante), usá random.sample() para elegir 4.
-#    - Combiná ambos grupos y retorná la lista de 8.
+def generar_mix_repaso(usuario_id, categoria_id):
+    pendiente_historial = obtener_listas_dashboard(usuario_id, categoria_id)
+    temas_fijos = pendiente_historial['historial'][0:4]
+    temas_azar = pendiente_historial['historial'][4:]
+    temas_a_elegir = min(4, len(temas_azar))
+    temas_azar_salida = random.sample(temas_azar, temas_a_elegir)
+    temas_fijos_azar_salida = temas_fijos + temas_azar_salida
+    return temas_fijos_azar_salida
