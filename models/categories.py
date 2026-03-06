533c6bdb35939cb0a3b0ee59bdf78c1d779a3260
Mariano Marra <ar.mmarra@gmail.com>
2026-03-06 13:25:20 -0300

    refact: categories.py to sqlalchemy 2.0

--- models/categories.py
+++ models/categories.py
@@ -1,18 +1,9 @@
 from models.database import Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String
 
 class Categoria(Base):
     __tablename__ = 'categorias'
-    id = Column(
-        'id_categoria',
-        Integer,
-        primary_key=True
-    )
-    nombre = Column(
-        'nombre_categoria',
-        String(50),
-        unique=True,
-        nullable=False
-    )
-    temas_relationship = relationship('Topic', back_populates='categorias_relationship')
+    id: Mapped[int] = mapped_column('id_categoria', primary_key=True)
+    nombre: Mapped[str] = mapped_column('nombre_categoria', String(50), unique=True, nullable=False)
+    temas_relationship: Mapped[list["Topic"]] = relationship('Topic', back_populates='categorias_relationship')
