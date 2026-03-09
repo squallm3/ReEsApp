489abfde78728f00e0c9d49fa3ca0d47face4c4a
Mariano Marra <ar.mmarra@gmail.com>
2026-03-09 13:46:55 -0300

    feat: migration topics.py to sqlalchemy 2.0

--- models/topics.py
+++ models/topics.py
@@ -1,35 +1,17 @@
 from models.database import Base
-from sqlalchemy import Column, Integer, String, Text, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Integer, String, Text, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from typing import Optional
 
 class Topic(Base):
     __tablename__ = 'temas'
-    id = Column(
-        'id_tema',
-        Integer,
-        primary_key=True
-    )
-    sesion = Column(
-        'nombre_sesion',
-        String(100),
-        unique=True,
-        nullable=False
-    )
-    contenido = Column(
-        'contenido',
-        Text,
-        nullable=False
-    )
-    comandos = Column(
-        'comandos',
-        Text
-    )
-    categoria = Column(
-        'id_categoria',
-        Integer,
-        ForeignKey('categorias.id_categoria')
-    )
-    progresos_relationship = relationship('Progreso', back_populates='tema_relationship', cascade='all, delete-orphan')
-    categorias_relationship = relationship('Categoria', back_populates='temas_relationship')
+    id: Mapped[int] = mapped_column('id_tema', Integer, primary_key=True)
+    sesion: Mapped[str] = mapped_column('nombre_sesion', String(100), unique=True, nullable=False)
+    contenido: Mapped[str] = mapped_column('contenido', Text, nullable=False)
+    comandos: Mapped[Optional[str]] = mapped_column('comandos', Text, nullable=True)
+    categoria: Mapped[int] = mapped_column('id_categoria', Integer, ForeignKey('categorias.id_categoria'))
+
+    progresos_relationship: Mapped[list["Progreso"]] = relationship('Progreso', back_populates='tema_relationship', cascade='all, delete-orphan')
+    categorias_relationship: Mapped["Categoria"] = relationship('Categoria', back_populates='temas_relationship')
 
 
